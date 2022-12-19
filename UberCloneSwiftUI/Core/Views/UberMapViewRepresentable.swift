8d2a394911f1ff83ae21acbce68d759679d897b0
JubelHossain <jubel_hossain@appnap.io>
2022-12-19 16:13:29 +0600

    location manager delegate function added

--- UberCloneSwiftUI/Core/Views/UberMapViewRepresentable.swift
+++ UberCloneSwiftUI/Core/Views/UberMapViewRepresentable.swift
@@ -12,6 +12,7 @@ struct UberMapViewRepresentable: UIViewRepresentable {
     let mapView = MKMapView()
     let locationManager = LocationManager()
     func makeUIView(context: Context) -> some UIView {
+        mapView.delegate = context.coordinator
         mapView.isRotateEnabled = false
         mapView.showsUserLocation = true
         mapView.userTrackingMode = .follow
@@ -34,6 +35,15 @@ extension UberMapViewRepresentable {
         init(parent: UberMapViewRepresentable) {
             self.parent = parent
             super.init()
+        }
+        
+        func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+            let region = MKCoordinateRegion(
+                center: CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude),
+                span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+            )
+            parent.mapView.setRegion(region, animated: true)
+            
         }
     }
 }
--- UberCloneSwiftUI/Managers/LocationManager.swift
+++ UberCloneSwiftUI/Managers/LocationManager.swift
@@ -11,8 +11,16 @@ class LocationManager: NSObject, ObservableObject {
     private let locationManager = CLLocationManager()
     
     override init() {
+        super.init()
+        locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
+        locationManager.stopUpdatingLocation()
+    }
+}
+extension LocationManager: CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard !locations.isEmpty else {return}
         locationManager.stopUpdatingLocation()
     }
 }
