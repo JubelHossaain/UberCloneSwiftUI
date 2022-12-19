9df91bed0b3284ab0b16f80290ca91e1ef7d129c
JubelHossain <jubel_hossain@appnap.io>
2022-12-19 15:24:56 +0600

    initial project set up

---
+++ UberCloneSwiftUI/UberCloneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneSwiftUIApp.swift
+//  UberCloneSwiftUI
+//
+//  Created by Appnap WS13 on 12/19/22.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
